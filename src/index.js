58f688528984e67b1f72dd3d58a0d74a472ddb06
Jacob Bare <jacob@limit0.io>
2018-07-29 10:00:44 -0500

    Start express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.info('Hello world!');
+const env = require('./env');
+const output = require('./output');
+const pkg = require('../package.json');
+const { app } = require('./express');
+
+const { PORT } = env;
+
+const server = app.listen(PORT);
+output.write(`Express app '${pkg.name}' listening on port ${PORT}`);
+
+module.exports = server;
